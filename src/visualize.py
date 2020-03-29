1054c8a624c093bac173138960e76774f6ab861b
ameyxd <ameyambade@gmail.com>
2020-03-28 22:29:38 -0500

    Visualize single day geo data

--- src/visualize.py
+++ src/visualize.py
@@ -3,18 +3,24 @@ import pandas as pd
 import os
 import plotly.plotly as py
 import plotly.figure_factory as ff
+import plotly
 import geopandas
 import shapely
+
+plotly.tools.set_credentials_file(username='ameyxd', api_key='BAEriF4kIYIK9OaznqUi')
 
 
 c = pd.read_csv(os.getcwd() + "/data/" + "us-counties.csv")
 s = pd.read_csv(os.getcwd() + "/data/" + "us-states.csv")
+c.fillna(0, inplace=True)
+s.fillna(0, inplace=True)
 
 texas_c = c[c.state == 'Texas']
 
-date = '2020-03-04'
+date = '2020-03-20'
 
 us_day = c[c.date == date]
+us_day = us_day[us_day.fips.notna()]
 fips = us_day.fips.tolist()
 values = us_day.cases.tolist()
 
@@ -27,9 +33,9 @@ colorscale = [
     'rgb(65, 53, 132)'
 ]
 
-fig = ff.create_choropleth(fips=fips, values=values, binning_endpoints=[1, 10, 30, 60, 100, 150],
+fig = ff.create_choropleth(fips=fips, values=values, binning_endpoints=[1, 10, 30, 60, 100],
                            colorscale=colorscale, county_outline={'color': 'rgb(255,255,255)', 'width': 0.5},
-                           round_legend_values=True, legend_title='COVID-19 cases ono March 3rd 2020',
+                           round_legend_values=True, legend_title='COVID-19 cases on March 3rd 2020',
                            title='United States Counties')
 
-py.iplot(fig, filename='united_states_outlines')
+py.plot(fig, filename='united_states_outlines')
